917374c9f9b27e2ae7b50505b0b720ecf1f18d96
vishal-viji <vishalshanmugam928@gmail.com>
2025-11-24 22:13:03 +0530

    Update App.js

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './components/Screens/Home';
+import Header from './components/Screens/Header';
+
+function App() {
+  return (
+   <>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+   </>
+  )
+}
+
+export default App
 
