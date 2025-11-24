ff158f90dfbf6fb256aaa2ca916815590e2243cc
vishal-viji <vishalshanmugam928@gmail.com>
2025-11-24 22:16:35 +0530

    Change import file extensions from .js to .jsx

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Home from './components/Screens/Home';
-import Header from './components/Screens/Header';
+import Home from './components/Screens/Home.jsx';
+import Header from './components/Screens/Header.jsx';
 
 function App() {
   return (
