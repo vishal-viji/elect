b5872a87165d1c3164c63030d18bf1981261d65d
vishal-viji <vishalshanmugam928@gmail.com>
2025-11-28 17:07:10 +0530

    Refactor imports in App.js for consistency

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-<<<<<<< HEAD
 import Home from './components/screens/Home.jsx';
 import LoginScreen from './components/screens/LoginScreen.jsx';
 import EditApplicant from './components/screens/EditApplicant.jsx';
 import Stats from './components/screens/Stats.jsx';
-=======
 import Home from './components/screens/Home';
 import Header from './components/screens/Header';
->>>>>>> 3d31305 (Update frontend and remove README)
+
 
 function App() {
   return (
@@ -24,9 +22,5 @@ function App() {
 }
 
 export default App
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 3d31305 (Update frontend and remove README)
 
